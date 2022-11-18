e50a43f99189806c6e7d6e87550739c5f2424de9
witarsahm <aw.marcel@gmail.com>
2022-11-18 15:50:34 +0700

    adding requirements for the packages and adding record_mic.py

---
+++ record_mic.py
@@ -0,0 +1,37 @@
+import pyaudio
+import wave
+
+p = pyaudio.PyAudio()
+
+# Constants
+CHUNK = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+SECONDS = 5
+
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=CHUNK,
+)
+
+print("start recording ...")
+frames = []
+for i in range(0, int(RATE / CHUNK * SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+print("stop recording ...")
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+with wave.open('output_by_marcel.wav', 'wb') as wf:
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
